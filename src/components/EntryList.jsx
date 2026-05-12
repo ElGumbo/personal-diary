af1c0bb8745b8653579e9baf52b5c67c10b4e7e9
Gumba <jeromontz@hotmail.de>
2026-05-12 10:28:48 +0200

    add entry modal

--- src/components/EntryList.jsx
+++ src/components/EntryList.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import EntryCard from "./EntryCard";
+import EntryModal from "./EntryModal";
 
 export default function EntryList({ entries }) {
+  const [selectedEntry, setSelectedEntry] = useState(null);
+
   return (
     <div className='grid grid-cols-[repeat(auto-fill,minmax(24rem,1fr))] gap-6 p-20'>
       {entries.map((entry) => (
-        <EntryCard key={entry.date} entry={entry} />
+        <EntryCard
+          key={entry.date}
+          entry={entry}
+          onClick={() => setSelectedEntry(entry)}
+        />
       ))}
+
+      {selectedEntry && (
+        <EntryModal
+          entry={selectedEntry}
+          onClose={() => setSelectedEntry(null)}
+        />
+      )}
     </div>
   );
 }
